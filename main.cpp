9f3e8e8a624822db759c4fa810846f8036d90913
Luke Montalvo <lukemontalvo@gmail.com>
2015-06-18 16:36:57 -0500

    + Added Sound and Background classes * Fixed the way that Resource and ResourceList are handled by MetaResourceList

--- main.cpp
+++ main.cpp
@@ -1,22 +1,9 @@
 #include <stdio.h>
-#include <string>
-#include <iostream>
 
 #include "resource_structures.hpp"
 
-std::string array_to_string(char** array, int length) {
-	std::string output = "{\n";
-	for (int i=0; i<length; i++) {
-		output.append("\t");
-		output.append(array[i]);
-		output.append("\n");
-	}
-	output.append("}\n");
-	return output;
-}
-
 int main() {
-	Sprite test ("spr_test", "sprites/test.png");
+	Background test ("spr_test", "sprites/test.png");
 	test.print();
 	//printf("name: %s\npath: %s\n", test.get_name().c_str(), test.get_path().c_str());
 	//printf("amount: %d\npath: %s\n", resource_list.all.get_amount(), "a");//resource_list.sprites.get_resource(test.get_id()).get_path().c_str());
--- resource_structures.hpp
+++ resource_structures.hpp
@@ -1,13 +1,8 @@
+#include <iostream>
 #include <string>
 #include <map>
 
-class Resource {
-		//int global_id;
-	public:
-		Resource();
-		~Resource();
-		//int get_global_id();
-};
+class Resource {};
 class ResourceList {
 		std::map<int,Resource> resources;
 		int next_index;
@@ -20,21 +15,8 @@ class ResourceList {
 };
 class MetaResourceList {
 	public:
-		ResourceList sprites; //, all;
+		ResourceList sprites, sounds, backgrounds; //, all;
 } resource_list;
-
-Resource::Resource() {
-	/*global_id = resource_list.all.add_resource(*this);
-	if (global_id < 0) {
-		fprintf(stderr, "Failed to add global resource: %d\n", global_id);
-	}*/
-}
-Resource::~Resource() {
-	//resource_list.all.remove_resource(global_id);
-}
-/*int Resource::get_global_id() {
-	return global_id;
-}*/
 
 ResourceList::ResourceList() {
 	next_index = 0;
@@ -74,16 +56,22 @@ class Sprite: public Resource {
 		Sprite(std::string, std::string);
 		~Sprite();
 		int reset();
+		int print();
+		
 		int get_id();
 		std::string get_name();
 		std::string get_path();
+		int get_width();
+		int get_height();
+		int get_subimage_amount();
+		int get_origin_x();
+		int get_origin_y();
 		int set_name(std::string);
-		int load_image_path(std::string);
+		int load_path(std::string);
 		int set_origin_x(int);
 		int set_origin_y(int);
 		int set_origin_xy(int, int);
 		int set_origin_center();
-		int print();
 };
 Sprite::Sprite () {
 	id = resource_list.sprites.add_resource(*this);
@@ -101,7 +89,7 @@ Sprite::Sprite (std::string new_name, std::string path) {
 	
 	reset();
 	set_name(new_name);
-	load_image_path(path);
+	load_path(path);
 }
 Sprite::~Sprite() {
 	resource_list.sprites.remove_resource(id);
@@ -118,6 +106,22 @@ int Sprite::reset() {
 	
 	return 0;
 }
+int Sprite::print() {
+	std::cout <<
+	"Sprite { "
+	"\n	id		" << id <<
+	"\n	name		" << name <<
+	"\n	image_path	" << image_path <<
+	"\n	texture		" << "//" <<
+	"\n	width		" << width <<
+	"\n	height		" << height <<
+	"\n	subimage_amount	" << subimage_amount <<
+	"\n	origin_x	" << origin_x <<
+	"\n	origin_y	" << origin_y <<
+	"\n}\n";
+	
+	return 0;
+}
 int Sprite::get_id() {
 	return id;
 }
@@ -127,13 +131,28 @@ std::string Sprite::get_name() {
 std::string Sprite::get_path() {
 	return image_path;
 }
+int Sprite::get_width() {
+	return width;
+}
+int Sprite::get_height() {
+	return height;
+}
+int Sprite::get_subimage_amount() {
+	return subimage_amount;
+}
+int Sprite::get_origin_x() {
+	return origin_x;
+}
+int Sprite::get_origin_y() {
+	return origin_y;
+}
 int Sprite::set_name(std::string new_name) {
 	// Deny name change if game is currently running (?)
 	// if (!game.is_running()) {
 	name = new_name;
 	return 0;
 }
-int Sprite::load_image_path(std::string path) {
+int Sprite::load_path(std::string path) {
 	image_path = path;
 	// Load OpenGL texture
 	/* texture = 
@@ -161,19 +180,243 @@ int Sprite::set_origin_center() {
 	set_origin_y(height/2);
 	return 0;
 }
-int Sprite::print() {
+
+class Sound: public Resource {
+		// Add new variables to the print() debugging method
+		int id;
+		std::string name;
+		std::string sound_path;
+		int play_type; // In memory vs continuous
+		int channel_amount; // Mono vs stereo
+		int sample_rate;
+		int sample_format;
+		int bit_rate;
+	public:
+		Sound();
+		Sound(std::string, std::string);
+		~Sound();
+		int reset();
+		int print();
+		
+		int get_id();
+		std::string get_name();
+		std::string get_path();
+		int get_play_type();
+		int get_channel_amount();
+		int get_sample_rate();
+		int get_sample_format();
+		int get_bit_rate();
+		int set_name(std::string);
+		int load_path(std::string);
+		int set_play_type(int);
+		int set_channel_ammount(int);
+		int set_sample_rate(int);
+		int set_sample_format(int);
+		int set_bit_rate(int);
+};
+Sound::Sound () {
+	id = resource_list.sounds.add_resource(*this);
+	if (id < 0) {
+		fprintf(stderr, "Failed to add sound resource: %d", id);
+	}
+	
+	reset();
+}
+Sound::Sound (std::string new_name, std::string path) {
+	id = resource_list.sounds.add_resource(*this);
+	if (id < 0) {
+		fprintf(stderr, "Failed to add sound resource: %d", id);
+	}
+	
+	reset();
+	set_name(new_name);
+	load_path(path);
+}
+Sound::~Sound() {
+	resource_list.sounds.remove_resource(id);
+}
+int Sound::reset() {
+	name = "";
+	sound_path = "";
+	play_type = 0;
+	channel_amount = 1;
+	sample_rate = 44100;
+	sample_format = 16;
+	bit_rate = 192;
+	
+	return 0;
+}
+int Sound::print() {
 	std::cout <<
-	"Sprite { "
+	"Sound { "
 	"\n	id		" << id <<
 	"\n	name		" << name <<
-	"\n	image_path	" << image_path <<
-	"\n	texture		" << "//" <<
-	"\n	width		" << width <<
-	"\n	height		" << height <<
-	"\n	subimage_amount	" << subimage_amount <<
-	"\n	origin_x	" << origin_x <<
-	"\n	origin_y	" << origin_y <<
+	"\n	sound_path	" << sound_path <<
+	"\n	play_type	" << play_type <<
+	"\n	channel_amount	" << channel_amount <<
+	"\n	sample_rate	" << sample_rate <<
+	"\n	sample_format	" << sample_format <<
+	"\n	bit_rate	" << bit_rate <<
 	"\n}\n";
 	
+	return 0;
+}
+int Sound::get_id() {
+	return id;
+}
+std::string Sound::get_name() {
+	return name;
+}
+std::string Sound::get_path() {
+	return sound_path;
+}
+int Sound::get_play_type() {
+	return play_type;
+}
+int Sound::get_channel_amount() {
+	return channel_amount;
+}
+int Sound::get_sample_rate() {
+	return sample_rate;
+}
+int Sound::get_sample_format() {
+	return sample_format;
+}
+int Sound::get_bit_rate() {
+	return bit_rate;
+}
+int Sound::set_name(std::string new_name) {
+	// Deny name change if game is currently running (?)
+	// if (!game.is_running()) {
+	name = new_name;
+	return 0;
+}
+int Sound::load_path(std::string path) {
+	sound_path = path;
+	// Load OpenAL sound
+	/*
+	 * 
+	 */
+	return 0;
+}
+int Sound::set_play_type(int new_play_type) {
+	play_type = new_play_type;
+	return 0;
+}
+int Sound::set_channel_ammount(int new_channel_amount) {
+	channel_amount = new_channel_amount;
+	return 0;
+}
+int Sound::set_sample_rate(int new_sample_rate) {
+	sample_rate = new_sample_rate;
+	return 0;
+}
+int Sound::set_sample_format(int new_sample_format) {
+	sample_format = new_sample_format;
+	return 0;
+}
+int Sound::set_bit_rate(int new_bit_rate) {
+	bit_rate = new_bit_rate;
+	return 0;
+}
+
+class Background: public Resource {
+		// Add new variables to the print() debugging method
+		int id;
+		std::string name;
+		std::string background_path;
+		int width, height;
+		bool is_tiling;
+	public:
+		Background();
+		Background(std::string, std::string);
+		~Background();
+		int reset();
+		int print();
+		
+		int get_id();
+		std::string get_name();
+		std::string get_path();
+		int get_width();
+		int get_height();
+		bool get_is_tiling();
+		int set_name(std::string);
+		int load_path(std::string);
+		int set_is_tiling(bool);
+};
+Background::Background () {
+	id = resource_list.backgrounds.add_resource(*this);
+	if (id < 0) {
+		fprintf(stderr, "Failed to add background resource: %d", id);
+	}
+	
+	reset();
+}
+Background::Background (std::string new_name, std::string path) {
+	id = resource_list.backgrounds.add_resource(*this);
+	if (id < 0) {
+		fprintf(stderr, "Failed to add background resource: %d", id);
+	}
+	
+	reset();
+	set_name(new_name);
+	load_path(path);
+}
+Background::~Background() {
+	resource_list.backgrounds.remove_resource(id);
+}
+int Background::reset() {
+	name = "";
+	background_path = "";
+	is_tiling = false;
+	
+	return 0;
+}
+int Background::print() {
+	std::cout <<
+	"Background { "
+	"\n	id		" << id <<
+	"\n	name		" << name <<
+	"\n	background_path	" << background_path <<
+	"\n	is_tiling	" << is_tiling <<
+	"\n}\n";
+	
+	return 0;
+}
+int Background::get_id() {
+	return id;
+}
+std::string Background::get_name() {
+	return name;
+}
+std::string Background::get_path() {
+	return background_path;
+}
+int Background::get_width() {
+	return width;
+}
+int Background::get_height() {
+	return height;
+}
+bool Background::get_is_tiling() {
+	return is_tiling;
+}
+int Background::set_name(std::string new_name) {
+	// Deny name change if game is currently running (?)
+	// if (!game.is_running()) {
+	name = new_name;
+	return 0;
+}
+int Background::load_path(std::string path) {
+	background_path = path;
+	// Load OpenGL texture
+	/* texture = 
+	 * width = 
+	 * height = 
+	 */
+	return 0;
+}
+int Background::set_is_tiling(bool new_is_tiling) {
+	is_tiling = new_is_tiling;
 	return 0;
 }
